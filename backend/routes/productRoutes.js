017a2354d343fdf9aa57a9cbefd13eaed6e99f70
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-07-07 22:05:54 +0530

    Custom Error Middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.get('/', asyncHandler(async (req,res) => {
     const products = await Product.find({});
+    throw new Error('Some error')
     res.json(products);
 }))
 
@@ -14,10 +15,10 @@ router.get('/:id', asyncHandler(async(req,res) => {
 
     if(product){
        return res.json(product)
+    }else{
+        res.status(404);
+        throw new Error('Resource not Found')
     }
-
-    res.status(404).json({message: 'Product not found'})
-    
 }))
 
-export default router
+export default router 
