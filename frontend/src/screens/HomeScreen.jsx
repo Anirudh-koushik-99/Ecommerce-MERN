64b2b58082179b3c00f4e84bb49e2a832849bdf4
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-07-08 23:52:22 +0530

    productsApiSlice, get product endpoint

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,20 +1,16 @@
-import { useEffect, useState } from 'react'
+import { useGetProductsQuery } from '../slices/productsApiSlice'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
 
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const {data} = await axios.get('/api/products')
-      setProducts(data);
-    }
-    fetchProducts()
-  }, [])
+  const {data: products, isLoading, error} = useGetProductsQuery();
+  const dispatch = useDispatch()
+  
   return (
     <>
+      {isLoading ? (<h1>Loading....</h1>) : error ? (<div>{error?.data?.message || error.error}</div>) : (<>
       <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
@@ -22,7 +18,8 @@ const HomeScreen = () => {
                 <Product product={product}/>
             </Col>
         ))}
-      </Row>
+      </Row></>) }
+      
     </>
   )
 }
