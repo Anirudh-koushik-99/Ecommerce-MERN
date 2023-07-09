4e5e35fffc741eec3a89d992b0dee859d31e313c
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-07-09 19:17:10 +0530

    Loader, cartSlice & Reducer

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,25 +1,34 @@
-import { useGetProductsQuery } from '../slices/productsApiSlice'
-import { Row, Col } from 'react-bootstrap'
-import Product from '../components/Product'
-
+import { useGetProductsQuery } from "../slices/productsApiSlice";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const HomeScreen = () => {
-  const {data: products, isLoading, error} = useGetProductsQuery();
-  
+  const { data: products, isLoading, error } = useGetProductsQuery();
+
   return (
     <>
-      {isLoading ? (<h1>Loading....</h1>) : error ? (<div>{error?.data?.message || error.error}</div>) : (<>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product}/>
-            </Col>
-        ))}
-      </Row></>) }
-      
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant={"danger"}>
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
