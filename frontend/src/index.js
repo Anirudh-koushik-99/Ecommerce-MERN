875694731fc3e4a419ac1602ab839b3702f7e654
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-07-14 22:03:57 +0530

    Login Screen Frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,10 +16,12 @@ import reportWebVitals from "./reportWebVitals";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<App />}>
     <Route index={true} path="/" element={<HomeScreen />}/>
+    <Route path="/login" element={<LoginScreen />} />
     <Route path="/products/:id" element={<ProductScreen />} />
     <Route path="/cart" element={<CartScreen />} />
   </Route>
