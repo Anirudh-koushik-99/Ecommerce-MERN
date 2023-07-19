9322bd1cffcb1f54ca59bf19ed9652a1cdd4633f
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-07-19 20:46:08 +0530

    Checkout Steps component

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -4,19 +4,19 @@ import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import FormContainer from "../components/FormContainer";
-import {saveShippingAddress}  from "../slices/cartSlice.js";
+import { saveShippingAddress } from "../slices/cartSlice.js";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 const ShippingScreen = () => {
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
 
-    const cart = useSelector((state) => state.cart)
-    const {shippingAddress} = cart
-
-  const [address, setAddress] = useState(shippingAddress?.address || '');
-  const [city, setCity] = useState(shippingAddress?.city || '');
-  const [postalCode, setPostalCode] = useState(shippingAddress?.postalCode || '');
-  const [country, setCountry] = useState(shippingAddress?.country || '');
-
-
+  const [address, setAddress] = useState(shippingAddress?.address || "");
+  const [city, setCity] = useState(shippingAddress?.city || "");
+  const [postalCode, setPostalCode] = useState(
+    shippingAddress?.postalCode || ""
+  );
+  const [country, setCountry] = useState(shippingAddress?.country || "");
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -24,12 +24,13 @@ const ShippingScreen = () => {
   const submitHandler = (e) => {
     e.preventDefault();
     console.log("Submit");
-    dispatch(saveShippingAddress({address, city, postalCode, country}))
-    navigate('/payment')
+    dispatch(saveShippingAddress({ address, city, postalCode, country }));
+    navigate("/payment");
   };
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2/>
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address" className="my-2">
@@ -68,7 +69,9 @@ const ShippingScreen = () => {
             onChange={(e) => setCountry(e.target.value)}
           ></Form.Control>
         </Form.Group>
-        <Button type="submit" variant="primary" className="my-2">Continue</Button>
+        <Button type="submit" variant="primary" className="my-2">
+          Continue
+        </Button>
       </Form>
     </FormContainer>
   );
