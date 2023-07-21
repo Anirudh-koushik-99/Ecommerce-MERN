652608a733f436910be64ceac41bde495b379d98
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-07-21 23:24:08 +0530

    Order Screen (frontend)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -21,7 +21,9 @@ import RegisterScreen from "./screens/RegisterScreen";
 import ShippingScreen from "./screens/ShippingScreen.jsx";
 import PaymentScreen from "./screens/PaymentScreen.jsx";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen.jsx";
+import OrderScreen from "./screens/OrderScreen.jsx";
 import PrivateRoute from "./components/PrivateRoute.jsx";
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -36,6 +38,8 @@ const router = createBrowserRouter(
         <Route path="/shipping" element={<ShippingScreen />} />
         <Route path="/payment" element={<PaymentScreen />} />
         <Route path="/placeorder" element={<PlaceOrderScreen />} />
+        <Route path="/order/:id" element={<OrderScreen />} />
+
       </Route>
 
     </Route>
