7d79321075549be28f7f0d242b3df8a494108c38
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-07-31 22:52:14 +0530

    Product review (frontend)

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -116,7 +116,7 @@ const createProductReview = asyncHandler(async (req, res) => {
       product.reviews.length;
 
     await product.save();
-    res.status(200).json({ message: "Review added" });
+    res.status(201).json({ message: "Review added" });
   } else {
     res.status(404);
     throw new Error("Resource not found");
