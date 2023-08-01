a62ea2e8405d4ffe2f4f0963594d5ab9afffa244
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-08-01 20:20:15 +0530

    Search Box

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,14 +6,19 @@ import Product from "../models/productModel.js";
 //Route GET /api/products
 //@access Public
 const getProducts = asyncHandler(async (req, res) => {
-    const pageSize = 3;
-    const page = Number(req.query.pageNumber) || 1;
-    const count = await Product.countDocuments();
+  const pageSize = 8;
+  const page = Number(req.query.pageNumber) || 1;
 
-  const products = await Product.find({})
-  .limit(pageSize)
-  .skip(pageSize*(page-1));
-  res.json({products, page, pages: Math.ceil(count / pageSize)});
+  const keyword = req.query.keyword
+    ? { name: { $regex: req.query.keyword, $options: 'i' } }
+    : {};
+
+  const count = await Product.countDocuments({...keyword});
+
+  const products = await Product.find({...keyword})
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
 
 //@desc Fetch single product
